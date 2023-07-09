b6757baf02eb5c90f8552f482ca6e641a1716cf7
stenioneves <stenioneves@ymail.com>
2023-07-09 11:52:27 -0300

    Inclusão do exercício 14

---
+++ unidade01/ex14.py
@@ -0,0 +1,12 @@
+#Elabore um programa que calcule e apresente o volume de uma caixa retangular, 
+#por meio da fórmula VOLUME = COMPRIMENTO * LARGURA * ALTURA.
+
+print("Calculo de volume  de um objeto retangular")
+comprimento=int(input("Digite o comprimento do objeto: "))
+largura=int(input("Digite a largura do objeto:"))
+altura=int(input("Digite a altura do objeto :"))
+#Simplificação
+print("O volume é ",comprimento*altura*largura,"M³")
+#Continuação do código de forma padrão
+volume= comprimento*largura*altura
+print(f"O volume é {volume} M³")
