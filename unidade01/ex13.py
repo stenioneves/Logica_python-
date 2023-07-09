62fd7180a340f80ee89ea1e299e93b14889755a7
stenioneves <stenioneves@ymail.com>
2023-07-09 13:38:07 -0300

    Inclusão do exercício 13

---
+++ unidade01/ex13.py
@@ -0,0 +1,3 @@
+#Elabore um programa que efetue a apresentação do valor da conversão em dólar de um valor lido em real. 
+#O programa deve solicitar a cotação do dólar e também a quantidade de reais disponível com o usuário, 
+#para que seja apresentado o valor em moeda americana.
