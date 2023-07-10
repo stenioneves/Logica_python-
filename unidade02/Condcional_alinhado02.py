0771a2fdc5e2a485ff1fe060efb9c7a99281e671
stenioneves <stenioneves@ymail.com>
2023-07-10 13:40:36 -0300

    Código finalizado

--- unidade02/Condcional_alinhado02.py
+++ unidade02/Condcional_alinhado02.py
@@ -1,3 +1,13 @@
-# Calcular se o aluno foi aprovado ou não tendo uma nota superior a 5 e frequencia superior a 75%
+# Calcular se o aluno foi aprovado ou não tendo uma nota superior a 5 e frequência superior a 75%
 
 nota=float(input("Digite a nota:"))
+frequencia=int(input("Digite sua frequência: "))
+
+if nota >= 5 and frequencia >= 75:
+    situacao= "aprovado"
+elif nota>=5 or frequencia >= 75:
+    situacao=" na recuperação"  
+else:
+    situacao="reprovado"   
+
+print(f"Você está {situacao}!")
