ef0635b1d05e7f5e558bcdc46273a6a8689f0197
stenioneves <stenioneves@ymail.com>
2023-07-10 12:35:44 -0300

    Conclusão do exercicio

--- unidade02/cenario.py
+++ unidade02/cenario.py
@@ -1,8 +1,14 @@
 #Cálculo de salário de um pessoa PJ, exercicio proposto em aula.
 # Bônus com de R$ 500 acima de 100h
+#Bloco de  controle
 
 
 total_horas=float(input("Digite a quantidade de horas trabalhadas: "))
 valor_horas=float(input("Digite o valor da hora de trabalho R$:"))
-bonus=0.00
-salario_horas=total_horas*valor_horas
+
+if(total_horas>= 100):
+    bonus= 500.0
+else:
+    bonus =0.0    
+salario=total_horas*valor_horas+bonus
+print(f"Seu salário é R$ : {salario}")
