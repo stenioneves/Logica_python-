ee303289b2e571fc7d24b54b79737dac1af143ac
stenioneves <stenioneves@ymail.com>
2023-07-13 14:36:32 -0300

    Exercicio de fatoração

---
+++ unidade02/fatoracao.py
@@ -0,0 +1,12 @@
+# Elaboração de um código  para fatoração
+
+numero=int(input("Digite um número: "))
+fatoracao=1 
+for i in range(numero):
+    fatoracao=fatoracao*(i+1)
+print(fatoracao)   
+
+
+
+ 
+ 
