3d3afd37a5804c4cb24398d19b920ce4e1409bef
stenioneves <stenioneves@ymail.com>
2023-07-13 11:04:59 -0300

    Fim do código da estrutura de repetição

--- unidade02/estrutura _de_repticaoFOR.py
+++ unidade02/estrutura _de_repticaoFOR.py
@@ -1,3 +1,9 @@
 #Estrutura de repetição FOR
+# Programa simples de Tabuada
 #Exercicio em aula.
 
+numero= int(input("Digite o numero :"))
+print(f"Tabuada do número : {numero}")
+for i in range(1,11): # range traz uma seguencia de números.
+    resultado= numero*i
+    print(f"{numero} X {i} = {resultado}")
