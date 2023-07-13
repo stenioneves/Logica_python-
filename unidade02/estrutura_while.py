7d5d11752ac7c2d43a71f67071f90010364baac8
stenioneves <stenioneves@ymail.com>
2023-07-13 12:14:00 -0300

    Exemplo do while

---
+++ unidade02/estrutura_while.py
@@ -0,0 +1,10 @@
+# Exercício em aula
+#Teste com  while, exemplo simples.
+
+
+senha=input("Digite sua senha:")
+
+while senha!= "134":
+    print("Senha incorreta! Tente novamente ...")
+    senha=input("Digite sua senha:")
+print("Você está autorizado, por enquanto ;)")
