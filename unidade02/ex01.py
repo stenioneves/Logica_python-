93f51feb298febcf87541ba105418fe80320b094
stenioneves <stenioneves@ymail.com>
2023-07-13 21:31:52 -0300

    Resolução do exercício 1

---
+++ unidade02/ex01.py
@@ -0,0 +1,7 @@
+#Escreva um programa que solicite um número ao usuário e verifique se ele é par ou ímpar.
+
+numero=float(input("Digite um número: "))
+if numero%2==0:
+    print("Esse número é par ")
+else:
+    print("Esse número é ímpar")    
