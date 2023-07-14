07372688fca57498808a203e98abe1fa0a855401
stenioneves <stenioneves@ymail.com>
2023-07-13 21:39:14 -0300

    Resolução do exercício 2

---
+++ unidade02/ex02.py
@@ -0,0 +1,10 @@
+#Faça um programa que solicite dois números ao usuário e exiba o maior deles.
+
+numero1=int(input("Digite o primeiro número:"))
+numero2=int(input("Digite o segundo número:"))
+if numero1>numero1:
+    print(f"O número {numero1} é maior que {numero2}")
+elif numero1==numero2:
+    print(f"Voce digitou o mesmo valor {numero1} para ambos!")  
+else:
+    print(f"O número {numero2} é maior que {numero1}")   
